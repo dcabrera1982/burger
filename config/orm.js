6ebcd331c783cbde7cc22dd7d3b0cf642b8047ba
David Cabrera <dcabrera1982@gmail.com>
2019-09-17 22:03:31 -0400

    final update to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,29 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+function createQmarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function translateSql(obj) {
+    var arr = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if(typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'" ;
+            }
+            arr.push(key + "=" + value)
+        }
+        
+    }
+    return arr.toString();
+}
+
 var orm = {
     selectAll: function (table, cb) {
         var dbQuery = "SELECT * FROM " + table + ";";
