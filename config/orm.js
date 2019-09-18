031f084ea928f8a0e2eb8866dfb769281a476698
David Cabrera <dcabrera1982@gmail.com>
2019-09-17 21:46:40 -0400

    input methods into orm.js

---
+++ config/orm.js
@@ -0,0 +1,45 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function (table, cb) {
+        var dbQuery = "SELECT * FROM " + table + ";";
+
+        connection.query(dbQuery, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        var dbQuery = "INSERT INTO " + table + " (" + cols.toString() + ") " + "VALUES (" + createQmarks(vals.length) + ") ";
+        console.log(dbQuery);
+        connection.query(dbQuery, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        var dbQuery = "UPDATE " + table + "SET" + translateSql(objColVals) + " WHERE " + condition;
+        console.log(dbQuery);
+        connection.query(dbQuery, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+    });
+},
+deleteOne: function(table, condition, cb) {
+    var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
+    console.log(dbQuery);
+        connection.query(dbQuery, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+});
+}
+};
