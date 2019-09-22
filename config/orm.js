7dd12e554c1cc5fe1dd97425538f6de82b1ae9fd
David Cabrera <dcabrera1982@gmail.com>
2019-09-22 17:23:09 -0400

    fixed issue with burger order

--- config/orm.js
+++ config/orm.js
@@ -37,7 +37,7 @@ var orm = {
     create: function (table, cols, vals, cb) {
         var dbQuery = "INSERT INTO " + table + " (" + cols.toString() + ") " + "VALUES (" + createQmarks(vals.length) + ") ";
         console.log(dbQuery);
-        connection.query(dbQuery, function (err, res) {
+        connection.query(dbQuery, vals, function (err, res) {
             if (err) {
                 throw err;
             }
