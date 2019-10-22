e974545a0c1b955818a5290254f5a059eb17d19e
David Cabrera <dcabrera1982@gmail.com>
2019-10-21 20:49:21 -0400

    changed db to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
 
 
-  connection = mysql.createConnection({
-  host: "localhost",
+
+//   connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
+
+connection = mysql.createConnection({
+  host: "tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  user: "xe90fa4ufomlmp91",
+  password: "qnki7cmjcseuh8q8",
+  database: "hxgmfrhaxdv0392d"
 });
-};
+
 
 // Make connection.
 connection.connect(function(err) {
