7840449da7597eb998ce3fed24945c4fa149c9f8
David Cabrera <dcabrera1982@gmail.com>
2019-10-29 21:38:12 -0400

    created new heroku

--- config/connection.js
+++ config/connection.js
@@ -4,21 +4,14 @@ var connection;
 
 
 
-//   connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "root",
-//   database: "burgers_db"
-// });
-
-connection = mysql.createConnection({
-  host: "tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  connection = mysql.createConnection({
+  host: "localhost",
   port: 3306,
-  user: "xe90fa4ufomlmp91",
-  password: "qnki7cmjcseuh8q8",
-  database: "hxgmfrhaxdv0392d"
+  user: "root",
+  password: "root",
+  database: "burgers_db"
 });
+
 
 
 // Make connection.
