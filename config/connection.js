86d0c43fae26e909dca97bc797d1286bf8b30dc8
David Cabrera <dcabrera1982@gmail.com>
2019-10-29 22:00:08 -0400

    removed extra connection work in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
-var connection;
 
 
 
 
-  connection = mysql.createConnection({
+
+ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
